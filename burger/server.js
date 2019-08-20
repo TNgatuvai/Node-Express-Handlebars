aeaadb99dd18a505049acbe6047c1031f09469fd
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-08-19 22:57:10 -0600

    Added Express handlebars & App.listen

--- burger/server.js
+++ burger/server.js
@@ -5,3 +5,12 @@ var db = require("./models")
 var PORT = process.env.PORT || 8001;
 
 var app = express();
+
+// configure Express Handlebars
+app.engine("handlebars", exphbs({ deafaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.listen(PORT, () => {
+    console.log("App now listening at http://localhost:" + PORT);
+});
