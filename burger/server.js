7e4f7d82a086f5d316bb3deb5a10a003e8557b38
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-08-19 23:53:24 -0600

    removed var db for now in order to run server.js

--- burger/server.js
+++ burger/server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var db = require("./models")
+// var db = require("./models")
 
 var PORT = process.env.PORT || 8001;
 
