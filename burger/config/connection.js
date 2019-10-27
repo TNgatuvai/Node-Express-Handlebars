e679f2be3a6069fb616676bcfe7dc704b4778f0b
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-10-26 22:15:04 -0600

    updated db connection

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,11 +1,16 @@
-var con = mysql.createConnection({
-    host: "",
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
     PORT: 3306,
     user: "root",
-    password: "root",
+    password: "",
     database: "burgers_db",
 });
 
-connection.connect();
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("connected as id: "+connection.threadid);
+});
 
 module.exports = connection;
