74f89a06dd95d31e871f4a174dbb57ab3ee6d675
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-10-29 01:19:20 -0500

    Added updates to routes

--- burger/controllers/routes.js
+++ burger/controllers/routes.js
@@ -1,25 +1,42 @@
-var express = require('express');
+var express = require("express");
+
 var router = express.Router();
-var burger = require('../models/burger');
 
-router.get('/', function(req, res){
-    burger.all(function(burger_data){
-        console.log(burger_data);
-        res.render('index');
-    })
-})
+var burgers = require("../models/burger.js").default;
 
-router.put('/burgers/update',function(req,res){
-    burger.update(req.body.burger_id, function(result){
-        console.log(result);
-        res.redirect('/');
-    });
+router.get("/", function(req,res){
+	res.redirect("burgers")
 });
 
-router.post('/burgers/create',function(req,res){
-    burger.create(req.body.burger_name, function(result){
-        res.redirect('/');
-    })
-})
+router.get("/burgers", function(req,res){
+	burgers.selectAll(function(data){
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/burgers/create", function(req,res){
+	burgers.insertOne([
+		"burger_name"
+		],[
+			req.body.burger_name
+			], function(data){
+				res.redirect("/burgers");
+			});
+});
+
+router.put("/burgers/update/:id", function(req,res){
+	var condition = "id = " + req.params.id;
+	console.log("condition", condition);
+
+	burgers.updateOne({
+		"devoured": req.body.devoured
+	}, condition, function(data){
+		res.redirect("/burgers")
+	});
+});
 
 module.exports = router;
