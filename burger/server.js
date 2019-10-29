21e0ab5d49d2ecff44249b8d0140bb0cd562c304
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-10-29 01:18:16 -0500

    Server.js updates

--- burger/server.js
+++ burger/server.js
@@ -1,23 +1,22 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var methodOverride = require("method-override");
+var app = express();
+var bodyParser = require("body-parser");
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
 var exphbs = require("express-handlebars");
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+var routes = require("./controllers/routes");
 
-app.use(methodOverride('_method'));
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.use("/", routes);
 
-var routes = require('./controllers/routes');
-app.use('/', routes);
-
-var port = 3000;
 app.listen(port);
