0fd5b9a1823bf664b3f34db53536c3a229e61765
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-10-29 01:37:27 -0500

    path update

--- burger/controllers/routes.js
+++ burger/controllers/routes.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-var burgers = require("../models/burger.js").default;
+var burgers = require("../models/burger.js");
 
 router.get("/", function(req,res){
 	res.redirect("burgers")
